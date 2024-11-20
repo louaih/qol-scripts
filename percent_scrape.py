ed29278b0594f24700e3b13e8c4d6a7fe8fff41b
tacslouai <lbh6976@nyu.edu>
2024-11-20 04:13:54 -0500

    add headers to marcus scraping

--- percent_scrape.py
+++ percent_scrape.py
@@ -24,8 +24,13 @@ def fetch_marcus_rate(url):
     try:
         # Define headers to mimic a browser
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Connection': 'keep-alive',
+            'Cookie': getenv('MARCUS_COOKIE')
         }
+
 
         # Send a GET request to the URL with headers
         response = requests.get(url, headers=headers)
